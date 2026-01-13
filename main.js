7f91166a83ffc0a094b43d71ac147053670a7719
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-01-13 22:01:23 +0000

    Started work on the Personal Blog page, creating a dropdown new entry form.

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ const hamburgerMenu = document.querySelector(".hamburger-menu"); // Runs a query
 hamburgerIcon.addEventListener("click", function () { // Listens for a click on the hamburger icon element
     hamburgerMenu.classList.toggle("dropdown-menu"); // When click detected, toggle .dropdown-menu class
 });
+
+/* New Entry Form Toggle */
+
+const newEntryButton = document.querySelector(".new-entry-btn");
+const newEntryForm = document.getElementById("new-blog-entry-form");
+const newEntryOpen = document.querySelector(".new-blog-entry");
+
+newEntryButton.addEventListener("click", function () {
+    newEntryOpen.classList.toggle("open-form");
+});
